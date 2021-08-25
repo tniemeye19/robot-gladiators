a618bd6bcfb27dbb733a19ba54727156e11facfd
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-08-24 20:49:05 -0400

    Blank/null response handling for the player-name prompt bug fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -50,7 +50,7 @@ var fight = function(enemy) {
             // Leave while() loop since enemy is dead
             break;
             } else {
-                window.alert(enemy.name + " still has " + enemyHealth + " health left.");
+                window.alert(enemy.name + " still has " + enemy.health + " health left.");
         }
 
         // Generate random damage value base on enemy's attack power
@@ -85,11 +85,11 @@ var startGame = function() {
 
             // Let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1) );
-
+            debugger;
             // Pick new enemy to fight based on the index of the enemy.names array
             var pickedEnemyObj = enemyInfo[i];
 
-            // Reset enemyHealth before starting new fight
+            // Reset enemy.health before starting new fight
             pickedEnemyObj.health = randomNumber(40, 60);
 
             // Pass the pickedenemy.name variable's value into the fight function, where it will assume the value of the enemy.name parameter
@@ -176,9 +176,21 @@ var shop = function() {
 
 // Game Information / Variables
 
+// Function to Set Name
+var getPlayerName = function() {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+};
+
 // Player Information
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
